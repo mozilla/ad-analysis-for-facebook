99fd2646b084d12cc31cd4073536188399a6bd56
Jason Chuang <jchuang@mozilla.com>
2018-10-30 12:10:46 -0400

    Updated GitHub Pages

---
+++ src/docs/monitor.js
@@ -0,0 +1,15 @@
+const Monitor = (function() {
+	const Monitor = function() {
+		const empty = function() {};
+		this.KEY_ENTER = empty;
+		this.KEY_EXIT = empty;
+		this.ENTER = empty;
+		this.EXIT = empty;
+		this.EVENT = empty;
+		this.OPTIONS = empty;
+		this.RESULTS = empty;
+		this.DATA = empty;
+		this.ASSERT = empty;
+	};
+	return Monitor;
+})();
